5274b28d555e0ca19ed53272cb3be928a2b267e5
Kapil Chaudhary <153476703+kapilchaudharygithub@users.noreply.github.com>
2024-04-17 14:43:32 +0530

    Update Navbar.js
    Adding proper comments in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom";
+// Importing necessary modules and components
+import React from "react"; // Importing React library
+import { Link } from "react-router-dom"; // Importing Link component for navigation
 
+// Navbar component for navigation
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid d-flex justify-content-center">
+        {/* Link to navigate to the home page */}
         <Link to="/" className="navbar-brand">
           Contact List - Kapil Chaudhary
         </Link>
@@ -13,4 +16,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default Navbar; // Exporting the Navbar component
