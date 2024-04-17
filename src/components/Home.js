33d234653cc4d0fe96a16e789773e914491d9378
Kapil Chaudhary <153476703+kapilchaudharygithub@users.noreply.github.com>
2024-04-17 14:44:04 +0530

    Update Home.js
    Adding proper comments in Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,35 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { toast } from "react-toastify";
+// Importing necessary modules and components
+import React from "react"; // Importing React library
+import { useDispatch, useSelector } from "react-redux"; // Importing useDispatch and useSelector hooks for Redux
+import { Link } from "react-router-dom"; // Importing Link component for navigation
+import { toast } from "react-toastify"; // Importing toast for notifications
 
+// Home component for displaying contacts
 const Home = () => {
+  // Accessing contacts from Redux store state
   const contacts = useSelector((state) => state);
+  const dispatch = useDispatch(); // Accessing Redux dispatch function
 
-  const dispatch = useDispatch();
-
+  // Function to delete a contact
   const deleteContact = (id) => {
-    dispatch({ type: "DELETE_CONTACT", payload: id });
-    toast.success("Contact deleted successfully!");
+    dispatch({ type: "DELETE_CONTACT", payload: id }); // Dispatching action to delete contact
+    toast.success("Contact deleted successfully!"); // Showing success notification
   };
 
+  // Rendering the main layout of the home page
   return (
     <div className="container">
       <div className="row">
+        {/* Button to navigate to the AddContact page */}
         <div className="col-md-12 my-5 text-end">
           <Link to="/add" className="btn btn-outline-dark">
             Add Contact
           </Link>
         </div>
+        {/* Table to display contacts */}
         <div className="col-md-10 mx-auto">
           <table className="table table-hover">
+            {/* Table header */}
             <thead className="text-dark bg-light text-center">
               <tr>
                 <th scope="col">#</th>
@@ -32,13 +39,16 @@ const Home = () => {
                 <th scope="col">Action</th>
               </tr>
             </thead>
+            {/* Table body */}
             <tbody>
+              {/* Mapping through contacts to display each contact */}
               {contacts.map((contact, id) => (
                 <tr key={id}>
                   <td>{id + 1}</td>
                   <td>{contact.name}</td>
                   <td>{contact.email}</td>
                   <td>{contact.number}</td>
+                  {/* Buttons to edit and delete a contact */}
                   <td>
                     <Link
                       to={`/edit/${contact.id}`}
@@ -64,4 +74,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default Home; // Exporting the Home component
